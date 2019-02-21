a5b8954f84e6d79161f95ff433e21fac508d0050
alexanderliljeborg <alexanderliljeborg@192.168.1.196>
2019-02-21 18:31:14 +0100

    Fixade paketstruktur

---
+++ Labb4/src/lab4/GomokuMain.java
@@ -0,0 +1,16 @@
+package lab4;
+
+import lab4.client.GomokuClient;
+import lab4.data.GomokuGameState;
+import lab4.gui.GomokuGUI;
+
+public class GomokuMain {
+
+	
+	//Lägg in argument som portnumber
+	public static void main(String[] portnumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

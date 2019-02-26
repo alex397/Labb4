19ac516e498191b845ec4b21cca048dc5e12e6fd
Krizzaan <47834268+Krizzaan@users.noreply.github.com>
2019-02-26 11:12:06 +0100

    Update GamePanel.java

--- Labb4/src/lab4/gui/GamePanel.java
+++ Labb4/src/lab4/gui/GamePanel.java
@@ -16,7 +16,7 @@ import lab4.data.GameGrid;
 
 public class GamePanel extends JPanel implements Observer{
 
-	private final int UNIT_SIZE = 20;
+	public final int UNIT_SIZE = 20;
 	private GameGrid grid;
 	
 	/**
@@ -27,10 +27,11 @@ public class GamePanel extends JPanel implements Observer{
 	public GamePanel(GameGrid grid){
 		this.grid = grid;
 		grid.addObserver(this);
-		Dimension d = new Dimension(grid.getSize()*UNIT_SIZE+1, grid.getSize()*UNIT_SIZE+1);
+		Dimension d = new Dimension(grid.getSize()*UNIT_SIZE, grid.getSize()*UNIT_SIZE);
 		this.setMinimumSize(d);
 		this.setPreferredSize(d);
 		this.setBackground(Color.WHITE);
+		this.setVisible(true);
 	}
 
 	/**
@@ -41,7 +42,10 @@ public class GamePanel extends JPanel implements Observer{
 	 * @param y the y coordinates
 	 * @return an integer array containing the [x, y] grid position
 	 */
-	public int[] getGridPosition(int x, int y){}
+	public int[] getGridPosition(int x, int y){
+		int [] arr = {x/UNIT_SIZE,y/UNIT_SIZE};
+		return arr; 
+	}
 	
 	public void update(Observable arg0, Object arg1) {
 		this.repaint();
@@ -49,7 +53,34 @@ public class GamePanel extends JPanel implements Observer{
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		for (int i = 0; i < grid.getSize(); i++) { // Y-led
+			for (int k = 0; k < grid.getSize(); k++) {// X-led
+					Rect(k, i, Color.BLACK, g);
+				if (grid.getLocation(k, i) == GameGrid.ME) {
+					Circle(k, i, Color.RED, g);
+				}
+				if (grid.getLocation(k, i) == GameGrid.OTHER) {
+					Cross(k, i, Color.BLUE, g);
+				}
+			}
+		}
+		this.repaint();
+	}
+	
+	public void Rect(int x, int y, Color color, Graphics g) {
+		g.setColor(color);
+		g.drawRect(x*UNIT_SIZE, y*UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+	}
+	
+	public void Circle(int x, int y, Color color, Graphics g) { //KAN KOMMA ATT BEHÖVA ÄNDRA STORLEKAR
+		g.setColor(color);
+		g.drawOval(x*UNIT_SIZE, y*UNIT_SIZE, UNIT_SIZE-2, UNIT_SIZE-2);
+	}	
+	public void Cross(int x, int y, Color color, Graphics g) { //KAN KOMMA ATT BEHÖVA ÄNDRA STORLEKAR
+		g.setColor(color);
+		g.drawLine(x*UNIT_SIZE, y*UNIT_SIZE, x*UNIT_SIZE, y*UNIT_SIZE);
+		g.drawLine(x*UNIT_SIZE, y*UNIT_SIZE + y, x*UNIT_SIZE + x, y*UNIT_SIZE);	
+	}	
 		
 	}
 	
-}
